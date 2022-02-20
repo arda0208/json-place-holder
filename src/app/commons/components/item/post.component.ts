cf3e4810e012041179281b17cc0e2496234db346
Arda Yucel <arda.yucel@accenture.com>
2022-02-20 19:56:01 +0300

    Coloring added.

--- src/app/commons/components/item/post.component.ts
+++ src/app/commons/components/item/post.component.ts
@@ -8,13 +8,26 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class PostComponent implements OnInit {
   @Input() public post!: PostItem;
+  @Input() public index: number;
   public showUserId: boolean = false;
+  public randomColor: string = '';
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.generateRandomColor();
+  }
 
   public toggleShowUserId(): void {
     this.showUserId = !this.showUserId;
+  }
+
+  private generateRandomColor(): void {
+    var red = Math.floor(255 - (255 * this.index) / 100);
+    var blue = Math.floor((255 * this.index) / 100);
+    // we format to css value and return
+    this.randomColor = 'rgb(' + red + ',0,' + blue + ')';
+    // this.randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    //this.randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
   }
 }
